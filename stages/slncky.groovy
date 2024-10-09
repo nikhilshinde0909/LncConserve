c302d9452489ee483d37a14a9337da0a48a7c7bc
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2024-10-09 18:41:48 +0530

    Update slncky.groovy

--- stages/slncky.groovy
+++ stages/slncky.groovy
@@ -1,35 +1,35 @@
 /***********************************************************
- ** Stages run lnc RNA analysis with slncky with python 3
+ ** Stages run lncRNA analysis with slncky using Python 3
  ** Author: Nikhil Shinde <sd1172@srmist.edu.in>
  ** Last Update: 30/09/2024
  *********************************************************/
 
-//Python 2.7 and slncky
-slncky_dir="slncky_out"
+//Python 3 and slncky
+slncky_dir = "slncky_out"
 
 prepare_annotations_bed = {
-	output.dir=slncky_dir
-	if (liftover = "" && noncoding = ""){
-	produce("Ref_genome.protein_coding.bed","Rel_ref_genome.protein_coding.bed"){
-	exec """
-	$python3 $ensembl_gtf2bed $annotation Ref_genome.prefix.prefix ;
- 	$python3 $annotation_related_species $annotation Ref_genome.prefix.prefix
-	"""
-	}
-	} else {
-	produce("Ref_genome.protein_coding.bed","Rel_ref_genome.protein_coding.bed"){
-	exec """
-	$gffread $annotation --bed -o ${output.dir}/temp.bed ;
-	cut -f1-12 ${output.dir}/temp.bed > $output1 && rm ${output.dir}/temp.bed ;
-	$gffread $annotation_related_species --bed -o ${output.dir}/temp1.bed ;
-        cut -f1-12 ${output.dir}/temp1.bed > $output2 && rm ${output.dir}/temp1.bed ;
-	"""
-	}
-	}
+    output.dir = slncky_dir
+    if (liftover == "" && noncoding == "") {
+        produce("Ref_genome.protein_coding.bed", "Rel_ref_genome.protein_coding.bed") {
+            exec """
+            $python3 $ensembl_gtf2bed $annotation Ref_genome.prefix.prefix ;
+            $python3 $annotation_related_species $annotation Ref_genome.prefix.prefix
+            """
+        }
+    } else {
+        produce("Ref_genome.protein_coding.bed", "Rel_ref_genome.protein_coding.bed") {
+            exec """
+            $gffread $annotation --bed -o ${output.dir}/temp.bed ;
+            cut -f1-12 ${output.dir}/temp.bed > $output1 && rm ${output.dir}/temp.bed ;
+            $gffread $annotation_related_species --bed -o ${output.dir}/temp1.bed ;
+            cut -f1-12 ${output.dir}/temp1.bed > $output2 && rm ${output.dir}/temp1.bed ;
+            """
+        }
+    }
 }
 
 fasta_index = {
-    if ('${genome}.fai' != "" && '${genome_related_species}.fai' != ""){
+    if ('${genome}.fai' != "" && '${genome_related_species}.fai' != "") {
         exec """
         $samtools faidx ${genome} ;
         $samtools faidx ${genome_related_species}
@@ -40,23 +40,23 @@ fasta_index = {
 }
 
 prepare_Liftover = {
-	output.dir = slncky_dir
-	if (liftover = ""){
-	produce(org_name+"to"+rel_sp_name+".over.chain.gz"){
-	exec """
- 	$python3 $Liftover $threads $genome $org_name $genome_related_species $rel_sp_name $distance ;
-  	mv ${output.dir}/${org_name}.${org_name}.over.chain.gz $output
-   	"""
-	} else {
-	exec "echo 'Liftover files are provided for conservation studies'"
-	}
-	}
+    output.dir = slncky_dir
+    if (liftover == "") {
+        produce(org_name + "to" + rel_sp_name + ".over.chain.gz") {
+            exec """
+            $python3 $Liftover $threads $genome $org_name $genome_related_species $rel_sp_name $distance ;
+            mv ${output.dir}/${org_name}.${org_name}.over.chain.gz $output
+            """
+        }
+    } else {
+        exec "echo 'Liftover files are provided for conservation studies'"
+    }
 }
-  	
+
 annotation_config = {
     output.dir = slncky_dir
-	if (liftover != "" && noncoding != ""){ 
-	from("Ref_genome.protein_coding.bed","Rel_ref_genome.protein_coding.bed") produce("annotation.config") {
+    if (liftover != "" && noncoding != "") { 
+        from("Ref_genome.protein_coding.bed", "Rel_ref_genome.protein_coding.bed") produce("annotation.config") {
             exec """
             echo '>'$org_name >> $output ;
             echo 'CODING='$input1 >> $output ;
@@ -74,11 +74,11 @@ annotation_config = {
             echo 'MIRNA='$rel_mir >> $output ;
             echo 'SNORNA='$rel_sno >> $output
             """
-	}
-        } else {
-	from("Ref_genome.protein_coding.bed","Ref_genome.noncoding.bed","Ref_genome.miRNA.bed","Ref_genome.snoRNA.bed","Rel_ref_genome.protein_coding.bed","Rel_ref_genome.noncoding.bed","Rel_ref_genome.miRNA.bed","Rel_ref_genome.snoRNA.bed",org_name+"to"+rel_sp_name+".over.chain.gz") produce("annotation.config"){
+        }
+    } else {
+        from("Ref_genome.protein_coding.bed", "Ref_genome.noncoding.bed", "Ref_genome.miRNA.bed", "Ref_genome.snoRNA.bed", "Rel_ref_genome.protein_coding.bed", "Rel_ref_genome.noncoding.bed", "Rel_ref_genome.miRNA.bed", "Rel_ref_genome.snoRNA.bed", org_name + "to" + rel_sp_name + ".over.chain.gz") produce("annotation.config") {
             exec """
-	    echo '>'$org_name >> $output ;
+            echo '>'$org_name >> $output ;
             echo 'CODING='$input1 >> $output ;
             echo 'GENOME_FA='$genome >> $output ;
             echo 'ORTHOLOG='$rel_sp_name >> $output ;
@@ -93,39 +93,40 @@ annotation_config = {
             echo 'NONCODING='$input6 >> $output ;
             echo 'MIRNA='$input7 >> $output ;
             echo 'SNORNA='$input8 >> $output
-	"""
+            """
         }
-	}
+    }
 }
 
 putative_lnc_npcts_bed = {
-	output.dir=slncky_dir
-	from("Putative.lnc_NPCTs.gtf") produce("Putative-lnc-nptcs.bed"){
-	exec """
-	$gffread $input --bed -o ${output.dir}/temp.bed ;
-	cut -f1-12 ${output.dir}/temp.bed > $output
-	"""
-	  }
+    output.dir = slncky_dir
+    from("Putative.lnc_NPCTs.gtf") produce("Putative-lnc-nptcs.bed") {
+        exec """
+        $gffread $input --bed -o ${output.dir}/temp.bed ;
+        cut -f1-12 ${output.dir}/temp.bed > $output
+        """
+    }
 }
 
 run_slncky = {
-	output.dir=slncky_dir
-	from("annotation.config","Putative-lnc-nptcs.bed") produce("slncky_out.lncs.info.txt"){
-	exec """
-	source $Activate slncky ;
-	$slncky -n $threads -c $input1 $input2 $org_name $slncky_options $output.prefix.prefix.prefix
-	"""
-	  }
+    output.dir = slncky_dir
+    from("annotation.config", "Putative-lnc-nptcs.bed") produce("slncky_out.lncs.info.txt") {
+        exec """
+        source $Activate slncky ;
+        $slncky -n $threads -c $input1 $input2 $org_name $slncky_options $output.prefix.prefix.prefix
+        """
+    }
 }
 
 ortholog_search = {
-	output.dir = slncky_dir
-	from("annotation.config", "Putative-lnc-nptcs.bed") produce(rel_sp_name+".orthologs.top.txt"){
+    output.dir = slncky_dir
+    from("annotation.config", "Putative-lnc-nptcs.bed") produce(rel_sp_name + ".orthologs.top.txt") {
         exec """
-        source ${Activate} slncky
+        source ${Activate} slncky ;
         $slncky -n $threads -c $input1 $input2 $slncky_ortho_options $org_name $output.prefix.prefix.prefix
         """
-    	}
+    }
 }
 
-slncky_run = segment { prepare_annotations_bed + fasta_index + prepare_Liftover + annotation_config + putative_lnc_npcts_bed + run_slncky + ortholog_search }
+slncky_run = segment { prepare_annotations_bed + fasta_index + prepare_Liftover + annotation_config + 
+	putative_lnc_npcts_bed + run_slncky + ortholog_search }
